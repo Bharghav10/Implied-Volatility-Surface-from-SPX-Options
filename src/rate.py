0b00049e7ceaca4238aa6a6843c20dd34dc508d4
Bharghava Dhumale <137162981+Bharghav10@users.noreply.github.com>
2026-09-10 23:26:09 -0400

    Update rate.py

--- src/rate.py
+++ src/rate.py
@@ -1,1 +1,16 @@
+"""
+Functions for working with CME SR1 / SOFR interest rates.
+"""
 
+
+def sr1_price_to_rate(price):
+    """
+    Convert an SR1 futures price into an implied
+    interest rate.
+
+    SR1 futures price convention:
+
+        Implied rate (%) = 100 - futures price
+    """
+
+    return 100 - price
